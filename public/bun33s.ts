5020d75ca5cee2c6b4a13834c52ddff78bfe8894
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-01-31 23:25:16 -0500

    cleanup

--- public/bun33s.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d06a6fbe0713f3cc2e24032dd7b218df5cea3e855ef5715b28a333eadcf45b0f
-size 1900868
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
 import { FC } from 'react'
 
-const Index: FC = () => <h1>👋 Hello</h1>
+const Index: FC = () => {
+  return (
+    <>
+      <fieldset>
+        <legend>Look at this video</legend>
+        <video controls>
+          <source src="/videos/bun33s.mp4" type="video/mp4" />
+        </video>
+      </fieldset>
+      <style jsx global>
+        {`
+          html,
+          body {
+            background: #333;
+            color: white;
+          }
+        `}
+      </style>
+    </>
+  )
+}
 
 export default Index
